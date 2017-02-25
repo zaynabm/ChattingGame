172eb1570172ec38be7523f2945f0d8c806e13c5
AyaMagdyMohamed <aya.magdy948@yahoo.com>
2017-02-25 23:17:31 +0200

    db Template

--- chatTest.py
+++ chatTest.py
@@ -3,6 +3,7 @@ from datetime import datetime
 client = MongoClient('localhost', 27018)
 db = client.chatDB
 collection=db['users']
+groupCollection=db['groups']
 # result = db.createCollection("users")
 
 name="asmaa"
@@ -11,51 +12,67 @@ age=22
 id=2
 password=2
 #-------------add user (sign up)----------------
-addUser = db.users.insert_one(
-{
-        "_id":id,
-        "name": name,
-        "password":password,
-        "age": age,
-        "email":email,
-        "friends": [],
-        "Groups":[],
-        "img":"img",
-        "notification":[]
-}
-)
+# addUser = db.users.insert_one(
+# {
+#         "_id":id,
+#         "name": name,
+#         "password":password,
+#         "age": age,
+#         "email":email,
+#         "friends": [],
+#         "Groups":[],
+#         "img":"img",
+#         "notification":[]
+# }
+# )
 #-----------------sign in-----------------
 # db.users.update({"_id":3},
 #                           {"$set" : {"password":3}})
-name="aya"
-flag="false"
-userID=db.users.find_one({"name":name},{"_id":1})# 1 for true to get id only
-
-#print(userID)
-if not(userID is None):
-    #print("hi")
-    flag="true"
-    for key, val in userID.items():
-
-       userPassword=db.users.find_one({"_id":val},{"password":1,"_id":0})# 1 for true to get pwd only
-       print(userPassword)
-givenPass=1
-if (flag is "true"):
-   for key, val in userPassword.items():
-      #print("ho")
-      #print(val)
-      if(givenPass is val):
-         print("Right user")
-
+# name="aya"
+# flag="false"
+# userID=db.users.find_one({"name":name},{"_id":1})# 1 for true to get id only
+#
+# #print(userID)
+# if not(userID is None):
+#     #print("hi")
+#     flag="true"
+#     for key, val in userID.items():
+#
+#        userPassword=db.users.find_one({"_id":val},{"password":1,"_id":0})# 1 for true to get pwd only
+#        print(userPassword)
+# givenPass=1
+# if (flag is "true"):
+#    for key, val in userPassword.items():
+#       #print("ho")
+#       #print(val)
+#       if(givenPass is val):
+#          print("Right user")
+#
 
 #-------------- add friend----------------
-name="zaienab"
-userID=db.users.find({"name":name},{"_id":1})
-print(userID)
-for key, val in userID.items():
-        #print(val)
-        id=1
-        db.users.update(
-                   {"_id":id},
-                   {"$push":{"friends":val}}
-                )
+# name="zaienab"
+# userID=db.users.find({"name":name},{"_id":1})
+# print(userID)
+# for key, val in userID.items():
+#         #print(val)
+#         id=1
+#         db.users.update(
+#                    {"_id":id},
+#                    {"$push":{"friends":val}}
+#                 )
+#--------------------add group------------------
+id=1
+name="openSource"
+owner="aya"
+img="img"
+desc="just description"
+addGroup = db.groups.insert_one(
+{
+        "_id":id,
+        "name": name,
+        "owner":owner,
+        "members":[],
+        "img":"img",
+        "description":desc
+}
+)
