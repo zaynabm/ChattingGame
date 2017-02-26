47fbb620ad8d6b530e68bbfa094f3a8edacf9a47
AyaMagdyMohamed <aya.magdy948@yahoo.com>
2017-02-26 18:39:41 +0200

    db Queries

--- chatTest.py
+++ chatTest.py
@@ -33,7 +33,7 @@ password=2
 # flag="false"
 # userID=db.users.find_one({"name":name},{"_id":1})# 1 for true to get id only
 #
-# #print(userID)
+# print(userID)
 # if not(userID is None):
 #     #print("hi")
 #     flag="true"
@@ -48,9 +48,35 @@ password=2
 #       #print(val)
 #       if(givenPass is val):
 #          print("Right user")
-#
+
 
 #-------------- add friend----------------
+# name="asmaa"
+# userID=db.users.find_one({"name":name},{"_id":1})
+# print(userID)
+# for key, val in userID.items():
+#         #print(val)
+#         id=1 # a8yr el id
+#         db.users.update(
+#                    {"_id":id},
+#                    {"$push":{"friends":val}}
+#                 )
+#-----------------------find my friends----------------------
+# userName="aya"
+# myFriendsIDs=db.users.find_one({"name":userName},{"_id":0,"friends":1})
+# myFriendsVal=[]
+# myFriendsNames=[]
+# for key, myFriendsVal in myFriendsIDs.items():
+#          print(myFriendsVal)
+# for val in myFriendsVal:
+#     myFriendsObjects=db.users.find_one({"_id":val},{"name":1,"_id":0})
+#     print(myFriendsObjects)
+#     for  key, val in myFriendsObjects.items():
+#          myFriendsNames.append(val)
+# for val in myFriendsNames:
+#           print(val)
+
+#---------------------remove friend-----------------------
 # name="zaienab"
 # userID=db.users.find_one({"name":name},{"_id":1})
 # print(userID)
@@ -59,7 +85,7 @@ password=2
 #         id=1
 #         db.users.update(
 #                    {"_id":id},
-#                    {"$push":{"friends":val}}
+#                    {"$pull":{"friends":val}}
 #                 )
 #--------------------add group------------------
 # id=1
@@ -83,22 +109,14 @@ password=2
 # print(groupID)
 # for key, val in groupID.items():
 #         #print(val)
-#         userID=1
+#         userID=2 #givenID
 #         db.groups.update(
 #                    {"_id":val},
 #                    {"$push":{"members":userID}}
 #                 )
-
-#---------------------remove friend-----------------------
-# name="zaienab"
-# userID=db.users.find_one({"name":name},{"_id":1})
-# print(userID)
-# for key, val in userID.items():
-#         #print(val)
-#         id=1
 #         db.users.update(
-#                    {"_id":id},
-#                    {"$pull":{"friends":val}}
+#                    {"_id":userID},
+#                    {"$push":{"Groups":val}}
 #                 )
 #-------------------leave group----------------------
 # groupName="openSource"
@@ -106,8 +124,26 @@ password=2
 # print(groupID)
 # for key, val in groupID.items():
 #         #print(val)
-#         userID=1
+#         userID=2
 #         db.groups.update(
 #                    {"_id":val},
 #                    {"$pull":{"members":userID}}
 #                 )
+#         db.users.update(
+#                    {"_id":userID},
+#                    {"$pull":{"Groups":val}}
+#                 )
+#-------------------find my groups----------------------
+# userName="zaienab"
+# myGroupsIDs=db.users.find_one({"name":userName},{"_id":0,"Groups":1})
+# myGroupsVal=[]
+# myGroupsNames=[]
+# for key, myGroupsVal in myGroupsIDs.items():
+#          print(myGroupsVal)
+# for val in myGroupsVal:
+#     myGroupsObjects=db.groups.find_one({"_id":val},{"name":1,"_id":0})
+#     print(myGroupsObjects)
+#     for  key, val in myGroupsObjects.items():
+#          myGroupsNames.append(val)
+# for val in myGroupsNames:
+#           print(val)
